b6c32f1257bc0bce7e4e786a60e22427eeb4b7e6
WooGenius <woogenius@nhnnext.org>
2014-07-17 15:28:22 +0900

    first commit

---
+++ week2-JSON Serialization/week2-JSON Serialization/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  week2-JSON Serialization
+//
+//  Created by WooGenius on 7/17/14.
+//  Copyright (c) 2014 WooGenius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSString sampleJson
+        
+    }
+    return 0;
+}
+
